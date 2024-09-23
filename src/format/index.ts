d38cd806326a3b3a6974ce1436afde0f1e70bca8
guantao <1368004939@qq.com>
2024-09-23 16:50:32 +0800

    增加format

--- src/format/index.ts
+++ src/format/index.ts
@@ -1,10 +1,14 @@
 import { getObjType } from '../object';
 
-// 根据时区格式化时间
+/**
+ *
+ * @param date
+ * @param tz
+ * @returns
+ */
 export const formatTimeZone = (date: string | Date, tz: string): string =>
   new Date(date || Date.now()).toLocaleString('en-US', { timeZone: tz || 'Asia/Shanghai' });
 
-// 格式化时间 str yyyy-mm-dd hh:MM:ss.S
 interface Options {
   'm+': number;
   'd+': number;
@@ -14,22 +18,30 @@ interface Options {
   'q+': number;
   S: number;
 }
+
+/**
+ *
+ * @param format yyyy.mm.dd hh:MM:ss
+ * @param date
+ * @param tz
+ * @returns
+ */
 export const formatDate = (format?: string, date?: string | number | Date, tz?: string): string => {
   let fmt = format || 'yyyy.mm.dd hh:MM:ss';
   let de = date ? (getObjType(date, 'Date') ? (date as Date) : new Date(date)) : new Date();
   if (tz) de = new Date(de.toLocaleString('en-US', { timeZone: tz }));
   // let fmt = str;
   const options: Options = {
-    'm+': de.getMonth() + 1, //月份
-    'd+': de.getDate(), //日
-    'h+': de.getHours(), //小时
-    'M+': de.getMinutes(), //分
-    's+': de.getSeconds(), //秒
-    'q+': Math.floor((de.getMonth() + 3) / 3), // 季度
-    S: de.getMilliseconds(), //毫秒
+    'm+': de.getMonth() + 1,
+    'd+': de.getDate(),
+    'h+': de.getHours(),
+    'M+': de.getMinutes(),
+    's+': de.getSeconds(),
+    'q+': Math.floor((de.getMonth() + 3) / 3),
+    S: de.getMilliseconds(),
   };
   if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, `${de.getFullYear()}`.substring(4 - RegExp.$1.length));
-  for (let k in options) {
+  for (const k in options) {
     if (new RegExp(`(${k})`).test(fmt)) {
       const $1 = RegExp.$1;
       fmt = fmt.replace(
